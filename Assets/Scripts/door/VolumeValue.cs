3f6d4dd77bd6067127478f11011b2b5492088d22
Arthope <91090763+Arthope@users.noreply.github.com>
2022-07-11 18:07:41 +0200

    idit

---
+++ Assets/Scripts/door/VolumeValue.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class VolumeСhange : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private float _maxVolume = 1;
+    private float _musicVolume = 0.1f;
+    private float _minVolume = 0.1f;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        StartCoroutine(VolumeChange()); ;
+    }
+
+    private IEnumerator VolumeChange()
+    {
+        _audioSource.volume = _musicVolume;
+        while (true)
+        {
+            if (_musicVolume < _maxVolume)
+            {
+                _musicVolume = Mathf.MoveTowards(_minVolume, _maxVolume, _musicVolume * Time.deltaTime);
+            }
+
+            else if (_musicVolume > _maxVolume)
+            {
+                _musicVolume = Mathf.MoveTowards(_maxVolume, _minVolume, _musicVolume * Time.deltaTime);
+            }
+            yield return null;
+        }
+    }
+
+}
